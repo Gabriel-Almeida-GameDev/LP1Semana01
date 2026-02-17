b7160c334bc561504d2111a2321f27c4436b19c9
Gabriel Almeida <dotdlenildaed4@gmail.com>
2026-02-17 15:02:04 +0000

    Added Summaries

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+ /// <summary>
+ /// Class from MeuPrograma
+ /// </summary>
     public class Program
     {
+/// <summary>
+/// Main working branch
+/// </summary>
+/// <param name="args">Printing Method</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
