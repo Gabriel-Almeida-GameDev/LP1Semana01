2eab425ef37984295d31d88f0260809dc04c5da2
Gabriel Almeida <dotdlenildaed4@gmail.com>
2026-02-17 15:32:13 +0000

    Added Variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int var1 = 1;
+            float var2 = 1.111f;
+            uint var3 = U5;
+
+
+
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
