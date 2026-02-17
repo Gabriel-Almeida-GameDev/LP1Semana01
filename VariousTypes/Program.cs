3bf7a7b7b6c83ec8df2e9f9caf51801f80d8cea8
Gabriel Almeida <dotdlenildaed4@gmail.com>
2026-02-17 16:09:02 +0000

    Added chars

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -7,12 +7,18 @@ namespace VariousTypes
         private static void Main(string[] args)
         {
             int var1 = 1;
-            float var2 = 1.111f;
-            uint var3 = U5;
+            byte var2 = 64;
+            uint var3 = 5u;
+            Console.WriteLine(var1);
+            Console.WriteLine(var2);
+            Console.WriteLine(var3);
 
-
-
-            Console.WriteLine("Hello LP!");
+            char emoji1 = '\u2615';
+            char emoji2 = '\u2605';
+            char emoji3 = '\u260E';
+            Console.WriteLine(emoji1);
+            Console.WriteLine(emoji2);
+            Console.WriteLine(emoji3);
         }
     }
 }
